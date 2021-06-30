51c11f8161c9506b09d801674627f9d1357d634a
davidyoon <qqpo12@naver.com>
2021-06-30 21:55:12 +0900

    add images on UIImage object

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -8,18 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //add variables
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
     @IBOutlet var imgView: UIImageView!
     @IBOutlet var btnResize: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
         // Do any additional setup after loading the view.
     }
 
     @IBAction func btnResizeImage(_ sender: UIButton) {
+        
+        
     }
     
     @IBAction func switchResizeImage(_ sender: UISwitch) {
+        
+        
     }
 }
 
